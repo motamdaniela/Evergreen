b6275d9aa4cfb14b2b1a242d50efc1815c017b76
motadaniela <40210288@esmad.ipp.pt>
2022-12-07 16:57:16 +0000

    Co-authored-by: beatrizsfrodrigues <beatrizsfrodrigues@users.noreply.github.com>

--- evergreen/src/App.vue
+++ evergreen/src/App.vue
@@ -4,88 +4,34 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-  </header>
+  <v-layout>
+    <v-app-bar
+      color="transparent"
+    >
+      <!-- <template v-slot:prepend>
+        <v-app-bar-nav-icon>=</v-app-bar-nav-icon>
+      </template> -->
 
-  <RouterView />
+      <v-app-bar-title>
+        <img src="../assets/logored.svg" style="width: 15%; margin-top: 0.5%">
+      </v-app-bar-title>
+
+      <RouterLink to="/home">Home</RouterLink>
+      <RouterLink to="/login">Login</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <v-row dense>
+          <v-col
+            v-for="n in 8"
+            :key="n"
+            cols="3"
+          >
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-layout>
 </template>
-
-<style scoped>
-@font-face {
-        font-family: 'ABC-Regular';
-        src:  url('./assets/Quicksand-Regular.ttf') format('ttf'),
-              url('../media/font/ABCCameraPlain-Regular-Trial.woff') format('woff');
-
-        font-family: 'ABC-Medium';
-        src:  url('../media/font/ABCCameraPlain-Medium-Trial.woff2') format('woff2'),
-              url('../media/font/ABCCameraPlain-Medium-Trial.woff') format('woff');
-
-        font-family: 'ABC-Bold';
-        src:  url('../media/font/ABCCameraPlain-Bold-Trial.woff2') format('woff2'),
-              url('../media/font/ABCCameraPlain-Bold-Trial.woff') format('woff');
-
-      }
-
-
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
--- evergreen/src/views/LandingPage.vue
+++ evergreen/src/views/LandingPage.vue
@@ -1,32 +1,3 @@
 <template>
-    <v-layout>
-      <v-app-bar
-        color="transparent"
-      >
-        <!-- <template v-slot:prepend>
-          <v-app-bar-nav-icon>=</v-app-bar-nav-icon>
-        </template> -->
-
-        <v-app-bar-title>
-          <img src="../assets/logored.svg" style="width: 15%; margin-top: 0.5%">
-        </v-app-bar-title>
-
-        <RouterLink to="/home">Home</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </v-app-bar>
-
-      <v-main>
-        <v-container fluid>
-          <v-row dense>
-            <v-col
-              v-for="n in 8"
-              :key="n"
-              cols="3"
-            >
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-    </v-layout>
+    
 </template>

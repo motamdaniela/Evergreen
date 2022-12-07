0390cd16eb8f1da79dc8203ab5b4d39f62ec00f3
Daniela Moreira <40210349@esmad.ipp.pt>
2022-12-07 16:57:12 +0000

    :)

--- evergreen/src/router/index.js
+++ evergreen/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LandingPage from "../views/LandingPage.vue";
+import Login from "../views/Login.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: LandingPage,
+    },
+    {
+      path: "/",
+      name: "login",
+      component: Login,
     },
     {
       path: "/about",

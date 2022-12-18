70f606e7ab384fe37a7d4b8aa4334effeabae619
Daniela Moreira <40210349@esmad.ipp.pt>
2022-12-18 18:36:25 +0000

    landing page ^-^

--- evergreen/src/views/LandingPageView.vue
+++ evergreen/src/views/LandingPageView.vue
@@ -1,31 +1,47 @@
 <template>
-  <div id="firstdiv">
-    <h1>Tudo o que precisas para o teu percurso</h1>
-   <h1>no Eco-Escolas num só sítio com</h1>
-   <h1 class="underlineGreen">Evergreen</h1>
-   <button class="btn-page">Começa já</button>
+  <div class="div" id="firstdiv">
+    <div id="text0">
+      <h1>Tudo o que precisas para o teu percurso <br> no Eco-Escolas num só sítio com</h1>
+      <h1 class="gradientGreen padding"><span>Evergreen</span></h1>
+      <p>Damos-te as boas-vindas ao Evergreen e à comunidade Eco-Escolas! <br> Pronto para tornares o mundo mais verde?</p>
+      <button class="btn-page" id="btn0">Começar</button>
+    </div>
   </div>
-  <div id="div1">
-    <h1>Participa em atividades</h1>
-   <p>O projeto Eco-Escolas tem tantas atividades divertidas que não vais querer perder!
-Aposto que vais adorar pelo menos uma e, com sorte vais adorar todas! Mas para descobrires isso tens que te inscrever em pelo menos uma!</p>
-<button class="btn-page">Começa já</button>
+
+  <div class="midDiv" id="div1">
+    <div id="text1">
+      <h1 class="title">Participa em atividades</h1>
+      <p>O projeto Eco-Escolas tem tantas atividades divertidas que não vais querer perder!
+      Aposto que vais adorar pelo menos uma e, com sorte vais adorar todas! Mas para descobrires isso tens que te inscrever em pelo menos uma!</p>
+      <button class="btn-page" id="btn1">Começa já</button>
+    </div>
+    <img src="../assets/images/atividades.svg" id="img1">
   </div>
-  <div id="div2">
-    <h1>Regista Ocorrências</h1>
-   <p>Já alguma vez reparaste que a torneira da casa de banho da tua escola estava a pingar? Ou que a luz da sala de aula estava a piscar? Esta é a tua oportunidade de fazer algo acerca disso!
-Quando registas uma ocorrência passas a informação aos teus professores e docentes que podem ajudar a resolver o problema e evitar desperdício.</p>
-   <button class="btn-page">Começa já</button>
+
+  <div class="midDiv" id="div2">
+    <img src="../assets/images/ocorrencias.svg" id="img2">
+    <div id="text2">
+      <h1 class="title">Regista Ocorrências</h1>
+      <p>Já alguma vez reparaste que a torneira da casa de banho da tua escola estava a pingar? Ou que a luz da sala de aula estava a piscar? Esta é a tua oportunidade de fazer algo acerca disso!
+      Quando registas uma ocorrência passas a informação aos teus professores e docentes que podem ajudar a resolver o problema e evitar desperdício.</p>
+      <button class="btn-page" id="btn2">Começa já</button>
+    </div>
+    
   </div>
-  <div id="div3">
-    <h1>Completa missões</h1>
-   <p>Quando interages com a plataforma não estás só a ajudar o planeta, também estas a ganhar recompensas!
-Completa todas as missões que temos para ti e explora tudo o que o Eco-Escolas tem para ofereçer enquanto te divertes!</p>
-   <button class="btn-page">Começa já</button>
-  </div>
+
+  <div class="midDiv" id="div3">
+    <div id="text3">
+      <h1 class="title">Completa missões</h1>
+      <p>Quando interages com a plataforma não estás só a ajudar o planeta, também estas a ganhar recompensas!
+      Completa todas as missões que temos para ti e explora tudo o que o Eco-Escolas tem para ofereçer enquanto te divertes!</p>
+      <button class="btn-page" id="btn3">Começa já</button>
+    </div>
+    <img src="../assets/images/missoes.svg" id="img3">
+    </div>
+
   <div id="lastdiv">
-    <h1>Estás pronto para dar o próximo passo para ajudar o teu planeta?</h1>
-   <button class="btn-page">Começa já</button>
+    <h1>Estás pronto para dar o próximo passo para ajudar <br> o teu planeta?</h1>
+    <button class="btn-page" id="btn4">Começa já</button>
   </div>
   
 </template>

4ed16a7870b781332024160dadd2aed5adb8e267
beatrizsfrodrigues <40210313@esmad.ipp.pt>
2022-12-19 23:22:20 +0000

    uwu

--- evergreen/src/stores/Occurrence.js
+++ evergreen/src/stores/Occurrence.js
@@ -1,21 +1,38 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import {defineStore} from 'pinia'
+import {useLocalStorage, useSessionStorage ,useStorage } from '@vueuse/core'
 
-Vue.use(Vuex);
+export const useOccurrenceStore = defineStore('occurrence',{
+  state: () => ({
+    occurrences:useStorage('occurrences',[]),
+  }),
 
-export const store = new Vuex.Store({
-  state: {
-    id: 0,
-    date: "",
-    hour: "",
-    campus: [],
-    building: "",
-    floor: "",
-    classroom: "",
-    type: "",
-    description: "",
-    photo: "",
-    user: "",
-    state: "",
+  getters: {
+    getOccurrences(){
+      return this.occurrences;
+    }
+  },
+
+  actions: {
+    add(obj){
+      this.occurrences.push(obj)
+    },
+
   },
 });
+
+// export const store = new Vuex.Store({
+//   state: {
+//     id: 0,
+//     date: "",
+//     hour: "",
+//     campus: [],
+//     building: "",
+//     floor: "",
+//     classroom: "",
+//     type: "",
+//     description: "",
+//     photo: "",
+//     user: "",
+//     state: "",
+//   },
+// });
--- evergreen/src/views/OccurrenceView.vue
+++ evergreen/src/views/OccurrenceView.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
     <h1>Ocorrências</h1>
+    <v-form
+      ref="form"
+      v-model="valid"
+      lazy-validation
+      @submit.prevent="onSubmit"
+    >
+      Escola:
+      <input type="text" />
+      Bloco:
+      <input type="text" />
+      Espaço:
+      <input type="text" />
+      Tipo:
+      <input type="text" />
+      Descrição:
+      <input type="text" />
+
+      <button type="submit" class="btn-page">Submeter</button>
+    </v-form>
   </div>
 </template>
 

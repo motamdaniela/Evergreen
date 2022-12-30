02fefa0065520d33bdec91b0588998a3312cd0cb
beatrizsfrodrigues <40210313@esmad.ipp.pt>
2022-12-30 01:22:25 +0000

    form

--- evergreen/src/views/FormView.vue
+++ evergreen/src/views/FormView.vue
@@ -1,16 +1,20 @@
 <template>
-    <div>
-        <h1>Questionario</h1>
-
-    </div>
+  <div>
+    <h1>Questionario</h1>
+    <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLScbfUdEF9NvKkWi3wq2yZVx_C0LTDN6AfmkZLrHIxs_ELRWFQ/viewform?embedded=true"
+      width="1000"
+      height="8100"
+      frameborder="0"
+      marginheight="0"
+      marginwidth="0"
+      >A carregar…</iframe
+    >
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>

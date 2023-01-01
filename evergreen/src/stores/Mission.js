2d664b1472d51f91bc8d68864df018669852e2af
beatrizsfrodrigues <40210313@esmad.ipp.pt>
2023-01-01 18:52:36 +0000

    mission

--- evergreen/src/stores/Mission.js
+++ evergreen/src/stores/Mission.js
@@ -1,14 +1,41 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import {defineStore} from 'pinia'
+import {useLocalStorage, useSessionStorage ,useStorage } from '@vueuse/core'
 
-Vue.use(Vuex);
+export const useMissionStore = defineStore('mission',{
+  state: () => ({
+    missions:useStorage('missions',[]),
+  }),
 
-export const store = new Vuex.Store({
-  state: {
-    id: 0,
-    title: "",
-    description: "",
-    reward: "",
-    users: [],
+  getters: {
+    getMissions(){
+      return this.missions;
+    },
+
+  },
+
+  actions: {
+    addMission(obj){
+      this.missions.push(obj)
+    },
+
   },
 });
+
+
+
+// import Vue from "vue";
+// import Vuex from "vuex";
+
+// Vue.use(Vuex);
+
+// export const store = new Vuex.Store({
+//   state: {
+//     id: 0,
+//     title: "",
+//     description: "",
+//     reward: "",
+//     users: [['email',0,'state'],['email',0,'state']],
+//     max:1,
+//     redirect:'',
+//   },
+// });
--- evergreen/src/views/MissionsView.vue
+++ evergreen/src/views/MissionsView.vue
@@ -1,15 +1,35 @@
 <template>
-    <div>
-        <h1>Missoes</h1>
-    </div>
+  <h1>Missoes</h1>
+  <div v-for="mission in missions">
+    <h3>{{ mission.title }}</h3>
+    <p>{{ mission.description }}</p>
+    <button @click="redirect(mission.redirect)" class="btn-page">go</button>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import { RouterLink } from "vue-router";
+import { useMissionStore } from "@/stores/Mission";
+import { useUsersStore } from "@/stores/User";
+export default {
+  setup() {
+    const missionStore = useMissionStore();
+    const usersStore = useUsersStore();
+
+    return { missionStore, usersStore };
+  },
+  name: "Missions",
+  data() {
+    return {
+      missions: this.missionStore.getMissions,
+    };
+  },
+  methods: {
+    redirect(n) {
+      this.$router.push(n);
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>

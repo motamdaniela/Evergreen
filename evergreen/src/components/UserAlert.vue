38365ecc1e4e1d6d9f0edfba99fa12fc4fb18e48
motadaniela <40210288@esmad.ipp.pt>
2023-01-15 23:12:41 +0000

    admin users wip

---
+++ evergreen/src/components/UserAlert.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <v-alert
+      color="info"
+      title="Tonal"
+      variant="tonal"
+    >
+      <div>
+        Maecenas nec odio et ante tincidunt tempus. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. Curabitur turpis.
+      </div>
+
+      <v-divider class="my-4 bg-light-blue-lighten-4"></v-divider>
+
+      <div class="d-flex flex-row align-center justify-space-between">
+        <div>
+          Proin magna. Vivamus in erat ut urna cursus vestibulum. Etiam imperdiet imperdiet orci.
+        </div>
+
+        <v-btn
+          color="info"
+          variant="outlined"
+        >
+          Okay
+        </v-btn>
+      </div>
+    </v-alert>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- evergreen/src/views/UsersView.vue
+++ evergreen/src/views/UsersView.vue
@@ -3,7 +3,15 @@
   <button class="btn-page">Adicionar Admin</button>
   <button class="btn-page">Adicionar Utilizador</button>
   <div v-for="user in users">
-    <div class="board">
+    <div v-if="user.type != 'admin'" class="board">
+      <img src="../assets/images/joana.jpg" id="profilePic" />
+      <p>{{ user.name }}</p>
+      <p>{{ user.email }}</p>
+      <button class="btn-page" v-on:click="OpenAlert(user)">Editar</button>
+      <button class="btn-page">Bloquear</button>
+      <button class="btn-page">Eliminar</button>
+    </div>
+    <div v-if="user.type == 'admin'" class="board">
       <img src="../assets/images/joana.jpg" id="profilePic" />
       <p>{{ user.name }}</p>
       <p>{{ user.email }}</p>
@@ -16,6 +24,7 @@
 
 <script>
 import { useUsersStore } from "@/stores/User";
+import Alert from "../components/UserAlert.vue";
 
 export default {
   setup() {
@@ -27,6 +36,36 @@ export default {
     return {
       users: this.userStore.getUsers,
     };
+  },
+  methods: {
+    OpenAlert(user) {
+      `
+      <v-alert
+      color="info"
+      title="Tonal"
+      variant="tonal"
+    >
+      <div>
+        Maecenas nec odio et ante tincidunt tempus. Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero, non adipiscing dolor urna a orci. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. Curabitur turpis.
+      </div>
+
+      <v-divider class="my-4 bg-light-blue-lighten-4"></v-divider>
+
+      <div class="d-flex flex-row align-center justify-space-between">
+        <div>
+          Proin magna. Vivamus in erat ut urna cursus vestibulum. Etiam imperdiet imperdiet orci.
+        </div>
+
+        <v-btn
+          color="info"
+          variant="outlined"
+        >
+          Okay
+        </v-btn>
+      </div>
+    </v-alert>
+    `
+    }
   },
 };
 </script>

bf89c9143655bc4815a49224434a03a21234b116
motadaniela <40210288@esmad.ipp.pt>
2023-01-21 23:10:36 +0000

    progresso editar perfil

--- evergreen/src/views/ProfileView.vue
+++ evergreen/src/views/ProfileView.vue
@@ -1,6 +1,37 @@
 <template>
+<v-dialog v-model="dialog">
+      <div class="fieldPklight modal">
+      
+      <v-card>
+        <v-card-actions>
+                <button class="btnRound btnPk" @click="dialog = false">
+                    <img style=" width: 15px " src="../assets/icons/icones/close.svg"/>
+                </button>
+            </v-card-actions>
+        <v-card-text>
+          <label for="username" class="semiTitle">Novo username</label>
+          <br />
+          <input class="input" id="username" v-model="newUsername" type="password" />
+          <br />
+          <label for="pass" class="semiTitle">Nova palavra-passe</label>
+          <br />
+          <input class="input" id="pass" v-model="newPassword" type="password" />
+        </v-card-text>
+        <v-card-actions>
+          <button class="btn-page btnG" @click="Edit" >Guardar</button>
+        </v-card-actions>
+      </v-card>
+    </div>
+      
+    </v-dialog>
+
+
+
   <div>
     <img id="ProfilePic" :src="user.photo"/>
+    <img id="editIcon" src="/src/assets/icons/icones/editProfile.svg"
+    @click="dialog = true"
+     />
     <p class="Name"><b>{{ user.name }}</b></p>
     <p><b>{{ user.school }}</b></p>
     <p>{{ user.email }}</p>
@@ -34,6 +65,11 @@ export default {
     return {
       user: "",
       badges: [],
+      dialog: false,
+      newPassword: '',
+      newUsername: '',
+      newColor: '',
+      newShape: '',
     };
   },
   created() {
@@ -53,7 +89,15 @@ export default {
   methods: {
     givemetheanswer(){
       console.log(this.user.idRewards);
-    }
+    },
+    Edit() {
+      this.user.username = this.newUsername;
+      this.user.password = this.newPassword;
+      // this.user.color = this.newColor;
+      // this.user.shape = this.newShape;
+      this.dialog = false;
+      this.userStore.edit(JSON.stringify(this.user))
+    },
   },
 };
 </script>

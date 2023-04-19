db7c9a386693520f712292427792bf7621739b15
Beatriz <beatriz.sfrodrigues@gmail.com>
2023-04-19 09:59:25 +0100

    mais coisas

---
+++ express/models/users.model.js
@@ -0,0 +1,37 @@
+let users = [
+  {
+    type: "user",
+    id: 1,
+    email: "user@gmail.com",
+    username: "user",
+    name: "user",
+    password: "Esmad_2223",
+    school: "ESMAD",
+    previousLoginDate: 0,
+    loginDate: 0,
+    streak: 0,
+    received: false,
+    photo:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+    points: 0,
+    activities: 0,
+    occurences: 0,
+    rewards: [],
+    state: "active",
+    council: false,
+  },
+  {
+    type: "admin",
+    id: 2,
+    email: "admin@gmail.com",
+    username: "admin",
+    name: "admin",
+    password: "Esmad_2223",
+    data: "data",
+    state: "active",
+    photo:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+  },
+];
+
+module.exports = users;

bf5ffbaab3984cfdebc72c6b64b988751760e46d
Beatriz <beatriz.sfrodrigues@gmail.com>
2023-05-03 11:42:14 +0100

    uu

--- express/controllers/activities.controller.js
+++ express/controllers/activities.controller.js
@@ -1,10 +1,18 @@
 const db = require("../models");
 const Activity = db.activities;
+const User = db.users;
 
 // ? gets all activities
 exports.findAll = async (req, res) => {
+  let idT = req.query.idTheme;
+  // console.log(idTheme);
+  let condition = {};
+  if (idT != undefined) {
+    condition = { idTheme: idT };
+  }
+  // let condition = idTheme ? { idTheme: new RegExp(idTheme, "i") } : {};
   try {
-    let data = await Activity.find({});
+    let data = await Activity.find(condition);
 
     return res.status(200).json({
       success: true,
@@ -42,16 +50,35 @@ exports.findOne = async (req, res) => {
   }
 };
 
-// // Display list of all activities of a theme
-// exports.filter = (req, res) => {
-//   let acts = [];
-//   activities.forEach((activity) => {
-//     if (activity.idTheme == req.params.themeID) {
-//       acts.push(activity);
-//     }
-//   });
-//   res.json(acts);
-// };
+// ? subscribe / unsubscribe to activity
+exports.subscribe = async (req, res) => {
+  try {
+    const activity = await Activity.findById(req.params.activityID);
+    const user = await User.findById(req.params.userID);
+    if (activity === null)
+      return res.status(404).json({
+        success: false,
+        msg: `Cannot find any activity with ID ${req.params.activityID}`,
+      });
+    if (activity.users.find((u) => u == req.params.userID)) {
+      activity.users.splice(activity.users.indexOf(req.params.userID), 1);
+    } else {
+      activity.users.push(req.params.userID);
+    }
+    return res.json({ success: true, activity: activity });
+  } catch (err) {
+    if (err.name === "CastError") {
+      return res.status(400).json({
+        success: false,
+        msg: "id parameter is not a valid object id",
+      });
+    }
+    return res.status(500).json({
+      success: false,
+      msg: `error retrieving activity with ID ${req.params.activityID}`,
+    });
+  }
+};
 
 // // subscribe / unsubscribe activity
 // // user id 0 doesn't work

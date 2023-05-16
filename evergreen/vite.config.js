0f01db6141d174560de90b0102a7854cc83d91d2
Daniela <92992969+motadaniela@users.noreply.github.com>
2023-05-16 21:11:41 +0100

    crying rn :p

--- evergreen/vite.config.js
+++ evergreen/vite.config.js
@@ -1,7 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import viteCompression from 'vite-plugin-compression';
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 // export default defineConfig({

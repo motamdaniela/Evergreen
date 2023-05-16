282621416dd9f0171c9b9930713a2d5229d453e7
Daniela <92992969+motadaniela@users.noreply.github.com>
2023-05-16 21:16:34 +0100

    AHHHHH

--- evergreen/vite.config.js
+++ evergreen/vite.config.js
@@ -16,7 +16,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), viteCompression()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

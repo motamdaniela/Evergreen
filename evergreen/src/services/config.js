d2dae65d42e9055bc772eb6daf49c60dc47f4f31
Beatriz <beatriz.sfrodrigues@gmail.com>
2023-06-16 18:50:17 +0100

    deploy

--- evergreen/src/services/config.js
+++ evergreen/src/services/config.js
@@ -1,5 +1,5 @@
-// const API_URL = "https://pink-rhinoceros-tutu.cyclic.app";
-const API_URL = "http://localhost:5000";
+const API_URL = "https://joyous-fish-nightgown.cyclic.app";
+// const API_URL = "http://localhost:5000";
 export default API_URL;
 
 // module.exports = {

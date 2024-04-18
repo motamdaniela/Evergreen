0a25c38f29ee2ee49177ac53de1e9202ed901e32
motamdaniela <40210288@esmad.ipp.pt>
2024-04-18 12:33:53 +0100

    ahhhh

--- evergreen/vite.config.js
+++ evergreen/vite.config.js
@@ -16,7 +16,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/Evergreen/',
   plugins: [vue(), viteCompression()],
   resolve: {
     alias: {
